b102a8d04a8564d643ad285edd7da1dfcd59fb6b
583134485@qq.com <583134485@qq.com>
2018-06-15 12:53:14 +0800

    s

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 
 //config for web page
 var config =new Object();
-config.baseurl="http://localhost:8080/libraryone/";
-//config.baseurl="/services/libraryone/";
+//config.baseurl="http://localhost:8080/libraryone/";
+config.baseurl="/services/libraryone/";
